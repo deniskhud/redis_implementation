d59be6debdc28015f50dfe4ea7ee0464fe818970
Denis <tessladen@gmail.com>
2025-05-31 12:03:48 +0200

    change a server logic and fix small bugs

--- client.cpp
+++ client.cpp
@@ -58,7 +58,7 @@ static int32_t send_req(int fd, const uint8_t* text, size_t len) {
     }
 
     std::vector<uint8_t> wbuf;
-    buf_append(wbuf, (const uint8_t)&len, 4);
+    buf_append(wbuf, (const uint8_t*)&len, 4);
     buf_append(wbuf, text, len);
     return write_full(fd, wbuf.data(), wbuf.size());
 }
@@ -99,44 +99,6 @@ static int32_t read_res(int fd) {
     //do
 
     printf("len: %u data: %.*s\n", len, len < 100 ? len : 100, &rbuf[4]);
-    return 0;
-}
-
-static int32_t query(int fd, const char *text) {
-    uint32_t len = (uint32_t) strlen(text);
-    if (len > k_max_msg) {
-        return -1;
-    }
-
-    char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4); //assume little endian
-    memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_full(fd, wbuf, 4 + len)) return err;
-
-    //4 bytes header
-    char rbuf[4 + k_max_msg + 1];
-    errno = 0;
-    int32_t err = read_full(fd, rbuf, 4);
-    if (err) {
-        msg(errno == 0 ? "EOF" : "read() error");
-        return err;
-    }
-
-    memcpy(&len, rbuf, 4); //assume little endian
-    if (len > k_max_msg) {
-        msg("too long");
-        return -1;
-    }
-
-    //reply body
-    err = read_full(fd, &rbuf[4], len);
-    if (err) {
-        msg("read() error");
-        return err;
-    }
-
-    //do something
-    printf("server says: %.*s\n", len, &rbuf[4]);
     return 0;
 }
 
